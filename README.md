69cfcac804fd913fbd92b310e60eb241a45469e8
Juan Diego Trujillo Montoya <jd.trujillom@uniandes.edu.co>
2021-02-18 21:43:17 -0500

    Un nuevo cambio

--- README.md
+++ README.md
@@ -14,6 +14,23 @@ Para desplegar el servidor, seguir los siguientes pasos:
 
         pip3 install flask-marshmallow
 
+        pip3 install flask_jwt_extended
+
+        pip3 install python-dateutil
+
 3. Ejecutar el archivo app.py
    
         python3 app.py
+
+# Al hacer un cambio en el modelo de datos
+Ejecutar las siguiente lineas:
+
+        python3
+
+        from app import db
+
+        db.drop_all()
+
+        db.create_all()
+
+        exit()
--- app.py
+++ app.py
@@ -1,136 +1,42 @@
-from flask import Flask, request
 from datetime import datetime
 import dateutil.parser
 
+from flask import Flask, request
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-
 from flask_marshmallow import Marshmallow
-
 from flask_restful import Api, Resource
-
+from flask_jwt_extended import JWTManager
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ThisIsHardestThing'
+app.config['JWT_SECRET_KEY'] = '!9m@S-dThyIlW[pHQbN^'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 Base = declarative_base()
-
-class Participacion(db.Model):
-    id_usuario = db.Column(db.Integer, ForeignKey('usuario.id'),primary_key = True)
-
-    id_concurso= db.Column(db.Integer, ForeignKey('concurso.id'),primary_key = True)
-
-    urlRecord=db.Column(db.String(255))
-
-    fechaPost = db.Column(db.DateTime)
-
-    estado = db.Column(db.String(50))
-
-class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    nombre = db.Column(db.String(50))
-
-    apellido = db.Column(db.String(50))
-
-    pssw = db.Column(db.String(50))
-
-    rol = db.Column(db.String(50))
-
-    children = relationship("Concurso")
-
-
-class Concurso(db.Model):
-
-    id = db.Column(db.Integer, primary_key = True)
-
-    nombre = db.Column( db.String(50) )
-
-    url = db.Column( db.String(255) )
-
-    fechaIni= db.Column(db.DateTime)
-
-    fechaFin = db.Column(db.DateTime)
-
-    value = db.Column(db.Integer)
-
-    guion = db.Column( db.String(255) )
-
-    recomendaciones = db.Column(db.String(255))
-
-    admin_id = Column(Integer, ForeignKey('usuario.id'))
-
-
-class Concurso_Schema(ma.Schema):
-    class Meta:
-        fields = ("id", "nombre", "url", "fechaIni", "fechaFin", "value", "guion", "recomendaciones")
-
-
-post_schema = Concurso_Schema()
-
-posts_schema = Concurso_Schema(many=True)
-
-class Usuario_Schema(ma.Schema):
-    class Meta:
-        fields = ("id", "nombre", "apellido", "pssw", "rol")
-
-post_schema = Usuario_Schema()
-
-posts_schema = Usuario_Schema(many=True)
-
-class Participacion_Schema(ma.Schema):
-    class Meta:
-        fields = ("urlRecord", "fechaPost", "estado")
-
-
-post_schema = Participacion_Schema()
-
-posts_schema = Participacion_Schema(many=True)
-
+jwt = JWTManager(app)
 api = Api(app)
 
-class RecursoListarConcursos(Resource):
+# Generating tables before first request is fetched
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-    def get(self):
+# Importing models and resources
+import models
+import resources
 
-        concursos = Concurso.query.all()
-
-        return posts_schema.dump(concursos)
-    def post(self):
-        nuevo_concurso = Concurso(
-
-            nombre=request.json['nombre'],
-
-            url=request.json['url'],
-
-            fechaIni=dateutil.parser.parse(request.json['fechaIni'], ignoretz=True),
-
-            fechaFin=dateutil.parser.parse(request.json['fechaFin'], ignoretz=True),
-
-            value=request.json['value'],
-
-            guion=request.json['guion'],
-
-            recomendaciones=request.json['recomendaciones']
-
-        )
-
-        db.session.add(nuevo_concurso)
-
-        db.session.commit()
-
-        return post_schema.dump(nuevo_concurso)
-
-
-api.add_resource(RecursoListarConcursos, '/concursos')
-
-
-
+# Api Endpoints
+api.add_resource(resources.RecursoListarConcursos, '/concursos')
+api.add_resource(resources.RecursoRegistro, '/registro')
+api.add_resource(resources.RecursoAutenticacion, '/login')
 
 if __name__ == '__main__':
 
