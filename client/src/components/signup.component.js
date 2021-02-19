452d9fc46832bb750563438567520a6e5d77a5e9
Juan Diego Trujillo Montoya <jd.trujillom@uniandes.edu.co>
2021-02-18 21:37:43 -0500

    Avances en frontend

--- client/src/components/signup.component.js
+++ client/src/components/signup.component.js
@@ -3,30 +3,34 @@ import React, { Component } from "react";
 export default class SignUp extends Component {
     render() {
         return (
-            <form>
-                <h3>Register</h3>
+            <div className="outer">
+                <div className="inner">
+                    <form>
+                        <h3>Register</h3>
 
-                <div className="form-group">
-                    <label>Nombre</label>
-                    <input type="text" className="form-control" placeholder="Juanito Pérez" />
+                        <div className="form-group">
+                            <label>Nombre</label>
+                            <input type="text" className="form-control" placeholder="Juanito Pérez" />
+                        </div>
+
+
+                        <div className="form-group">
+                            <label>Correo</label>
+                            <input type="email" className="form-control" placeholder="alguien@uniandes.edu.co" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Contraseña</label>
+                            <input type="password" className="form-control" placeholder="************" />
+                        </div>
+
+                        <button type="submit" className="btn btn-dark btn-lg btn-block">Enviar</button>
+                        <p className="forgot-password text-right">
+                            ¿Ya estás <a href="#">registrado?</a>
+                        </p>
+                    </form>
                 </div>
-
-
-                <div className="form-group">
-                    <label>Correo</label>
-                    <input type="email" className="form-control" placeholder="alguien@uniandes.edu.co" />
-                </div>
-
-                <div className="form-group">
-                    <label>Contraseña</label>
-                    <input type="password" className="form-control" placeholder="************" />
-                </div>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Enviar</button>
-                <p className="forgot-password text-right">
-                    ¿Ya estás <a href="#">registrado?</a>
-                </p>
-            </form>
+            </div>
         );
     }
 }
